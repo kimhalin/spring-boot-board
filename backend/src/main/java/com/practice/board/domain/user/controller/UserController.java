83f12d6acd4f517f5c5bb91981a55ec4b12b3efb
halin <halin130@naver.com>
2022-12-19 00:17:45 +0900

    feat: swagger 설정

--- backend/src/main/java/com/practice/board/domain/user/controller/UserController.java
+++ backend/src/main/java/com/practice/board/domain/user/controller/UserController.java
@@ -5,6 +5,13 @@ import com.practice.board.domain.user.dto.mapper.UserMapper;
 import com.practice.board.domain.user.dto.request.UserCreateRequestDto;
 import com.practice.board.domain.user.dto.response.BasicUserResponseDto;
 import com.practice.board.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +30,15 @@ public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
+    @Operation(summary = "post user", description = "user 생성하기")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED", content = @Content(schema = @Schema(implementation = BasicUserResponseDto.class)))
+    })
+    @Parameters({
+            @Parameter(name = "name"),
+            @Parameter(name = "email"),
+            @Parameter(name = "nickname")
+    })
     @PostMapping
     public ResponseEntity<BasicUserResponseDto> create(@Valid @RequestBody UserCreateRequestDto dto) {
         User entity = userService.create(dto);
